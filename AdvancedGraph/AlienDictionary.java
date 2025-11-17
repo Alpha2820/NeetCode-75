c5b0fa75f2b9455a859eb3faa8d283aeaeb5bbe0
Abhijeet Tiwari <abhijeett709@gmail.com>
2025-11-17 14:53:50 +0530

    Advanced Graph added and Alien Dictionary Problem solved

---
+++ AdvancedGraph/AlienDictionary.java
@@ -0,0 +1,80 @@
+package AdvancedGraph;
+
+import java.util.*;
+
+public class AlienDictionary {
+  public static String alienOrder(String[] words) {
+    Map<Character, Set<Character>> adjList = new HashMap<>();
+    int[] inDegree = new int[26];
+    Arrays.fill(inDegree, -1);
+
+    for (String word : words) {
+      for (char c : word.toCharArray()) {
+        adjList.putIfAbsent(c, new HashSet<>());
+        inDegree[c - 'a'] = 0;
+      }
+    }
+
+    for (int i = 0; i < words.length - 1; i++) {
+      String word1 = words[i];
+      String word2 = words[i + 1];
+      int minLength = Math.min(word1.length(), word2.length());
+      boolean foundDifference = false;
+
+      for (int j = 0; j < minLength; j++) {
+        char c1 = word1.charAt(j);
+        char c2 = word2.charAt(j);
+        if (c1 != c2) {
+          if (!adjList.get(c1).contains(c2)) {
+            adjList.get(c1).add(c2);
+            inDegree[c2 - 'a']++;
+          }
+          foundDifference = true;
+          break;
+        }
+      }
+
+      if (!foundDifference && word1.length() > word2.length()) {
+        return "";
+      }
+    }
+
+    Queue<Character> queue = new LinkedList<>();
+    for (char c : adjList.keySet()) {
+      if (inDegree[c - 'a'] == 0) {
+        queue.offer(c);
+      }
+    }
+
+    StringBuilder result = new StringBuilder();
+    while (!queue.isEmpty()) {
+      char current = queue.poll();
+      result.append(current);
+      for (char neighbor : adjList.get(current)) {
+        inDegree[neighbor - 'a']--;
+        if (inDegree[neighbor - 'a'] == 0) {
+          queue.offer(neighbor);
+        }
+      }
+    }
+
+    if (result.length() != adjList.size()) {
+      return "";
+    }
+
+    return result.toString();
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.nextLine();
+    String[] words = new String[n];
+    for (int i = 0; i < n; i++) {
+      words[i] = sc.nextLine();
+    }
+    String result = alienOrder(words);
+    System.out.println(result);
+    sc.close();
+  }
+}
